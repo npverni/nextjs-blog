929bbee2c3c3dcae4ba1bcabe2aadf21c946efe2
Nathan Verni <npverni@gmail.com>
2020-10-28 15:43:23 -0400

    wip

---
+++ pages/posts/first-post.js
@@ -0,0 +1,13 @@
+import Head from 'next/head';
+import Layout from '../../components/layout';
+
+const FirstPost = () => (
+  <Layout>
+    <Head>
+      <title>My Title</title>
+    </Head>
+    <h1>First Post</h1>
+  </Layout>
+)
+
+export default FirstPost;
